ddd29c3ca32c51d8a91410f34510c0dd2cf658ec
craig8588 <Craig8588@sina.com>
2013-07-31 14:29:47 +0800

    new single page view

---
+++ SinglePageView/SinglePageView/LEOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LEOAppDelegate.h
+//  SinglePageView
+//
+//  Created by Wang Leo on 13-7-31.
+//  Copyright (c) 2013年 Wang Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LEOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
